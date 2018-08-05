9803d9669542207d39150c9850315ee4bd98a4f4
prajwaldcunha <dcunhaprajwal@gmail.com>
2018-08-05 18:18:45 +0530

    Updated gradle files

--- mup/src/main/java/com/prajwaldcunha/mup/ApiCall.java
+++ mup/src/main/java/com/prajwaldcunha/mup/ApiCall.java
@@ -1,29 +1,24 @@
 package com.prajwaldcunha.mup;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
 
 
 public class ApiCall {
-    public static String GET(OkHttpClient client, String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        Response response = client.newCall(request).execute();
-        return response.body().string();
-    }
 
-    //POST network request
-    public static String POST(OkHttpClient client, String url, RequestBody body) throws IOException {
-        Request request = new Request.Builder()
+
+    public static String doOkHttpPost(String url, RequestBody body) throws IOException {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .writeTimeout(0, TimeUnit.SECONDS)
+                .build();
+        okhttp3.Request request = new okhttp3.Request.Builder()
                 .url(url)
                 .post(body)
                 .build();
-        Response response = client.newCall(request).execute();
+        okhttp3.Response response = client.newCall(request).execute();
         return response.body().string();
     }
 
