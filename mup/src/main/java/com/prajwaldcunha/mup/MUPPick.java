c84cba6e651770561983675c673af485f834be26
Royden Rego <RoydenRego@softrixz.com>
2018-08-05 17:54:49 +0530

    Added Singleton to MUPPick

--- mup/src/main/java/com/prajwaldcunha/mup/MUPPick.java
+++ mup/src/main/java/com/prajwaldcunha/mup/MUPPick.java
@@ -1,4 +1,16 @@
 package com.prajwaldcunha.mup;
 
+
 public class MUPPick {
+
+    private static MUPPick instance;
+
+    public static MUPPick init() {
+        if(instance == null) {
+            instance = new MUPPick();
+        }
+        return instance;
+    }
+
+
 }
